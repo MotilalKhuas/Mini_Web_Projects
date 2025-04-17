bd8ccadc923a67dcb1b931e093e17063332a9dae
MotilalKhuas <khuasmotilal57@gmail.com>
2025-04-17 19:08:32 +0530

    Load data for 5days forecast

--- Weather/script.js
+++ Weather/script.js
@@ -3,10 +3,11 @@
 const API_KEY = "9631287053ce83f36cd0a0ef5b0b4302";
 
 const CURRENT_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?"; //lat={lat}&lon={lon}&appid={API key}
+const FUTURE_WEATHER_URL = "https://api.openweathermap.org/data/2.5/forecast?"; // lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric
 const AIR_POLUTION_URL = "http://api.openweathermap.org/data/2.5/air_pollution?"; //lat={lat}&lon={lon}&appid={API key}
 const GEOENCOADING_URL = "http://api.openweathermap.org/geo/1.0/direct?q="
 
-function fillNowSectionData(weather_data){
+function fillNowSectionData(current_weather_data){
     const temp = document.querySelector("#current-temp");
     const status = document.querySelector("#weather-status");
     const date = document.querySelector("#current-date");
@@ -15,19 +16,41 @@ function fillNowSectionData(weather_data){
     const today = new Date();
     const date_str = `${today.toLocaleDateString('en-US', { weekday: 'long' })} ${today.getDate()}, ${today.toLocaleDateString('en-US', { month: 'long' })}`;
 
-    temp.innerHTML = `<p>${Math.round(weather_data.main.temp - 273.15)}<sup>&deg;C</sup></p>
-                          <img src="./Assets/${weather_data.weather[0].icon}.png" alt="${weather_data.weather[0].description} icon">`;
+    temp.innerHTML = `<p>${Math.round(current_weather_data.main.temp - 273.15)}<sup>&deg;C</sup></p>
+                          <img src="./Assets/${current_weather_data.weather[0].icon}.png" alt="${current_weather_data.weather[0].description} icon">`;
         
-    status.innerHTML =  `<p>${weather_data.weather[0].description}</p>`
+    status.innerHTML =  `<p>${current_weather_data.weather[0].description}</p>`
 
     date.innerHTML = `<i class="fa-regular fa-calendar"></i>
                         <p><time datetime="${today.toISOString().split('T')[0]}">${date_str}</time></p>`;
 
     loc.innerHTML = `<i class="fa-solid fa-location-dot"></i>
-                            <p>${weather_data.name}, ${weather_data.sys.country}</p>`;
+                            <p>${current_weather_data.name}, ${current_weather_data.sys.country}</p>`;
 }
 
-function fillHighlightSectionData(weather_data, air_pollution_data){
+function fill5dayForecastData(future_weather_data){
+
+    const day_wise_containers = document.querySelector("#forcast-weather-section > .section").children;
+    let count = 0;
+
+    for(let i = 7; i <= future_weather_data.list.length; i+=8){
+        const elem = day_wise_containers[count++];
+
+        const weather_img_container = elem.querySelector(".future-weather-img");
+        const temp_container = elem.querySelector(".future-temp-val");
+        const date_container = elem.querySelector(".future-date");
+        const day_container = elem.querySelector(".future-day");
+
+        const date = new Date(future_weather_data.list[i].dt * 1000);
+
+        weather_img_container.src = `./Assets/${future_weather_data.list[i].weather[0].icon}.png`;
+        temp_container.innerHTML = `${Math.round(future_weather_data.list[i].main.temp)}<sup>&deg;c</sup>`;
+        date_container.innerHTML = `${date.getDate()}  ${date.toLocaleDateString('en-US', { month: 'long' })}`;
+        day_container.innerHTML = `${date.toLocaleDateString('en-US', { weekday: 'long' })}`;
+    }
+}
+
+function fillHighlightSectionData(current_weather_data, air_pollution_data){
     const pm2_5 = document.querySelector("#particulate-matter-val");
     const so2 = document.querySelector("#sulfur-dioxide-val");
     const no2 = document.querySelector("#nitrogen_dioxide-val");
@@ -39,8 +62,8 @@ function fillHighlightSectionData(weather_data, air_pollution_data){
     const visibility = document.querySelector("#visibility-val");
     const feels_like_temp = document.querySelector("#feels-like-temp");
 
-    const sunrise = new Date(weather_data.sys.sunrise * 1000);
-    const sunset = new Date(weather_data.sys.sunset * 1000);
+    const sunrise = new Date(current_weather_data.sys.sunrise * 1000);
+    const sunset = new Date(current_weather_data.sys.sunset * 1000);
 
     pm2_5.innerHTML = `${air_pollution_data.list[0].components.pm2_5.toFixed(1)}`;
     so2.innerHTML = `${air_pollution_data.list[0].components.so2.toFixed(1)}`;
@@ -49,10 +72,10 @@ function fillHighlightSectionData(weather_data, air_pollution_data){
 
     sunrise_time.innerHTML = `${sunrise.getHours()} : ${sunrise.getMinutes()} <span class="highlight-matrices-units">am</span>`;
     sunset_time.innerHTML = `${sunset.getHours() % 12} : ${sunrise.getMinutes()} <span class="highlight-matrices-units">pm</span>`;
-    humidity.innerHTML = `${weather_data.main.humidity}<span class="highlight-matrices-units"> %</span>`;
-    pressure.innerHTML = `${weather_data.main.pressure}<span class="highlight-matrices-units"> hPa</span>`;
-    visibility.innerHTML = `${Math.round(weather_data.visibility / 1000)}<span class="highlight-matrices-units"> Km</span>`
-    feels_like_temp.innerHTML = `${Math.round(weather_data.main.feels_like-273.15)}<sup>&deg;<span>C</span></sup>`;
+    humidity.innerHTML = `${current_weather_data.main.humidity}<span class="highlight-matrices-units"> %</span>`;
+    pressure.innerHTML = `${current_weather_data.main.pressure}<span class="highlight-matrices-units"> hPa</span>`;
+    visibility.innerHTML = `${Math.round(current_weather_data.visibility / 1000)}<span class="highlight-matrices-units"> Km</span>`
+    feels_like_temp.innerHTML = `${Math.round(current_weather_data.main.feels_like-273.15)}<sup>&deg;<span>C</span></sup>`;
 }
 
 async function getCoordinates(loc){
@@ -80,15 +103,18 @@ function liveLocation(){
 async function loadAllData(lat, lon){
     const urls = [
         `${CURRENT_WEATHER_URL}lat=${lat}&lon=${lon}&appid=${API_KEY}`,
+        `${FUTURE_WEATHER_URL}lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`,
         `${AIR_POLUTION_URL}lat=${lat}&lon=${lon}&appid=${API_KEY}`
+       
     ]
 
     try{
         const responses = await Promise.all(urls.map(url=>fetch(url)));
         const data = await Promise.all(responses.map(response=>response.json()));
-        const [weather_data, air_polution_data] = data
-        fillNowSectionData(weather_data);
-        fillHighlightSectionData(weather_data, air_polution_data)
+        const [current_weather_data, future_weather_data,air_polution_data] = data
+        fillNowSectionData(current_weather_data);
+        fill5dayForecastData(future_weather_data);
+        fillHighlightSectionData(current_weather_data, air_polution_data)
     }
     catch(err){
         console.log(err.message);
